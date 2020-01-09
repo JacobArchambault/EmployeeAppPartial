5daaabcf754a7ff2848a36a0b86c40e9ac928011
Jacob Archambault <jacob.archambault@waystar.com>
2020-01-09 08:38:40 -0500

    Add project files.

---
+++ Employee.Core.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace EmployeeApp
+{
+    partial class Employee
+    {
+        // Field data.
+        private string empName;
+
+        #region Ctors
+        // Note use of constructor chaining.
+        public Employee() { }
+        public Employee(string name, int id, float pay)
+          : this(name, 0, id, pay)
+        { }
+        public Employee(string name, int age, int id, float pay)
+        {
+            Name = name;
+            Age = age;
+            ID = id;
+            Pay = pay;
+        }
+        #endregion
+
+        #region Properties 
+        // Properties!
+        public string Name
+        {
+            get { return empName; }
+            set
+            {
+                if (value.Length > 15)
+                    Console.WriteLine("Error!  Name length exceeds 15 characters");
+                else
+                    empName = value;
+            }
+        }
+
+        // We could add additional business rules to the sets of these properties,
+        // however there is no need to do so for this example.
+        public int ID { get; set; }
+        public float Pay { get; set; }
+        public int Age { get; set; }
+
+        public string SocialSecurityNumber { get; } = "";
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EmployeeApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Fun with Encapsulation *****\n");
+            Employee emp = new Employee("Marvin", 456, 30_000);
+            emp.GiveBonus(1000);
+            emp.DisplayStats();
+
+            // Set and get the Name property.
+            emp.Name = "Marv";
+            Console.WriteLine("Employee is named: {0}", emp.Name);
+            Console.ReadLine();
+
+        }
+    }
+}
